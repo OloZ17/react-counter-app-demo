2c98e3824e07647352c454a79d74bdad9b671e2d
Thomas Lamarche <glims@wanadoo.fr>
2021-11-21 18:26:20 +0100

    renaming

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import App from './app';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
